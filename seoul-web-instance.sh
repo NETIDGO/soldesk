74808196ab338ed4712bb6f76650b66c35f26365
Gum-mok Kim <134674780+netidgo@users.noreply.github.com>
2024-03-31 00:37:46 +0900

    Update seoul-web-instance.sh

--- seoul-web-instance.sh
+++ seoul-web-instance.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo echo "<h1>서울 웹 서버</h1>" > /var/www/html/index.html
+instance_id=$(ec2-metadata -i | cut -d " " -f 2)
+private_ip=$(ec2-metadata -o | cut -d " " -f 2)
+availability_zone=$(ec2-metadata -z | cut -d " " -f 2)
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+cat <<EOF > /var/www/html/index.html
+<h1>서울 웹 서버에 오신 것을 환영합니다.</h1>
+<h2>Instance ID: ${instance_id}</h2>
+<h2>Private IP: ${private_ip}</h2>
+EOF
 
