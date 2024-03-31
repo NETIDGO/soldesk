f77062b5e453ae3a3f209002d3491889a4285284
Gum-mok Kim <134674780+netidgo@users.noreply.github.com>
2024-04-01 02:01:46 +0900

    Update nfs-server.sh

--- aws/nfs-server.sh
+++ aws/nfs-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir /root/shared
-chmod 777 /root/shared
-echo "/root/shared 10.0.0.0/8(rw,sync)" > /etc/exports
+mkdir /nfs-shared
+chmod 777 /nfs-shared
+echo "/nfs-shared 10.0.0.0/8(rw,sync)" > /etc/exports
 systemctl start nfs-server
 systemctl enable nfs-server
