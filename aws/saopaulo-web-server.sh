c21267c20ffc76fd912b2850ee718ab1d40e7b78
Gum-mok Kim <134674780+netidgo@users.noreply.github.com>
2024-04-03 17:32:24 +0900

    Update saopaulo-web-server.sh

--- aws/saopaulo-web-server.sh
+++ aws/saopaulo-web-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-availability_zone=$(ec2-metadata -z | cut -d ' ' -f 2)
+availability_zone=$(ec2-metadata -z)
 instance_id=$(ec2-metadata -i)
 private_ip=$(ec2-metadata -o)
 yum install -y httpd
@@ -7,7 +7,7 @@ systemctl start httpd
 systemctl enable httpd
 cat <<EOF > /var/www/html/index.html
 <h1>Welcome to Saopaulo Web Server.</h1>
-<h3>가용영역: ${availability_zone}</h3>
+<h3>${availability_zone}</h3>
 <h3>${instance_id}</h3>
 <h3>${private_ip}</h3>
 EOF
